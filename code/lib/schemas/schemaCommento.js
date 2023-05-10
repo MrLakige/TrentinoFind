20874a49c656e665bbf74c0e39f84539d8dbeaf5
MrLakige <Callegari.Michele01@gmail.com>
2023-05-10 17:32:53 +0200

    modified:   code/lib/schemas/schemaCommento.js 	modified:   code/lib/schemas/schemaGiocatore.js 	new file:   code/view/css/stile.css 	modified:   code/view/html/home.html

--- code/lib/schemas/schemaCommento.js
+++ code/lib/schemas/schemaCommento.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 exports.user = new mongoose.Schema({
-    
+    idPlayer: String,
+    idObject: String,
+    date: Date,
+    comment: String
 });
--- code/lib/schemas/schemaGiocatore.js
+++ code/lib/schemas/schemaGiocatore.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 exports.user = new mongoose.Schema({
     idPlayer: String,
     Ranking: Number,
-    g,   
-    validated: Boolean,
-    comments: String,
-    phone: String,
-    type: String,
+    
+    
 });
