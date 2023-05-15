f857544f37b7cb389012690864df14762b6b6473
MrLakige <Callegari.Michele01@gmail.com>
2023-05-15 13:40:23 +0200

    modified:   code/app/app.js

--- code/app/app.js
+++ code/app/app.js
@@ -21,44 +21,6 @@ app.use((req,res,next) => {
 })
 
 
-/**
- * Resource routing
- */
-
-//app.use('/api/v1/utente', utente);
-
-app.use("/api/v1/giocatore",(req, res)=>{});
-
-//per la classe giocatore
-/*
-app.get("/api/v1/giocatore/{id}", (req, res))
-app.put("/api/v1/giocatore/{id}")
-app.delete("/api/v1/giocatore/{id}")
-app.set("/api/v1/giocatore")
-*/
-
-// return css file
-app.get("/view/css/stile.css",(req, res)=>{
-    console.log("send css file");
-    res.sendFile(path.join(__dirname, '../view/css/stile.css'));
-});
-
-
-// return home page
-app.get("/home",(req, res) => {
-    res.status(200);
-    console.log("send home page");
-    res.sendFile(path.join(__dirname, '../view/html/home.html'));
-});
-
-// return home page as default
-app.use("/",(req, res) => {
-    res.status(200);
-    console.log("send home page");
-    res.sendFile(path.join(__dirname, '../view/html/home.html'));
-});
-
-
 // Default 404 handler 
 app.use((req, res) => {
     res.status(404);
