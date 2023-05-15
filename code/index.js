e940dae4932a99a3df53cdb32d883719b7ac3214
MrLakige <Callegari.Michele01@gmail.com>
2023-05-15 13:37:07 +0200

    modified:   code/index.js

--- code/index.js
+++ code/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
  * Configure mongoose
  */
 // mongoose.Promise = global.Promise;
-app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+app.locals.db = mongoose.connect('mongodb+srv://2LM:passwordditest@trentinofind.x1ubooa.mongodb.net/TrentinoFind?retryWrites=true&w=majority')
 .then ( () => {
     
     console.log("Connected to Database");
