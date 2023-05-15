909544d16922ba78a68bb2795ee595be1b0ae3aa
MrLakige <Callegari.Michele01@gmail.com>
2023-05-15 13:29:59 +0200

    renamed:    code/lib/app.js -> code/app/app.js 	renamed:    code/lib/database/dbms.js -> code/app/database/dbms.js 	renamed:    code/lib/users/amministratore.js -> code/app/models/amministratore.js 	renamed:    code/lib/oggetti/commenti.js -> code/app/models/commenti.js 	renamed:    code/lib/users/giocatore.js -> code/app/models/giocatore.js 	renamed:    code/lib/users/moderatore.js -> code/app/models/moderatore.js 	renamed:    code/lib/oggetti/oggetto.js -> code/app/models/oggetto.js 	renamed:    code/lib/users/utente.js -> code/app/models/utente.js 	renamed:    code/lib/schemas/schemaCommento.js -> code/app/schemas/schemaCommento.js 	renamed:    code/lib/schemas/schemaGiocatore.js -> code/app/schemas/schemaGiocatore.js 	renamed:    code/lib/schemas/schemaOggetto.js -> code/app/schemas/schemaOggetto.js 	renamed:    code/lib/schemas/schemaUtente.js -> code/app/schemas/schemaUtente.js 	modified:   code/index.js

--- code/index.js
+++ code/index.js
@@ -1,5 +1,31 @@
-const app = require('./lib/app.js');
-const db = require('./lib/database/dbms.js');
+const app = require('./app/app.js');
+const mongoose = require('mongoose');
+
+/**
+ * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+ */
+const port = process.env.PORT || 8080;
+
+
+/**
+ * Configure mongoose
+ */
+// mongoose.Promise = global.Promise;
+app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});
+
+
+/*
+const app = require('./app/app.js');
+const db = require('./app/database/dbms.js');
 const port= process.env.PORT || 8080;
 
 db.start;
