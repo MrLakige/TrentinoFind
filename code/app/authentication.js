aec49a98c62d71a48bd26728ba8fe96f4e6a2f34
MrLakige <Callegari.Michele01@gmail.com>
2023-05-16 11:21:47 +0200

    new file:   code/app/authentication.js 	modified:   code/package-lock.json 	modified:   code/package.json

---
+++ code/app/authentication.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const utente = require('./models/schemaUtente'); // get our mongoose model
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const crypto = require('crypto');
+
+
+// ---------------------------------------------------------
+// route to authenticate and get a new token
+// ---------------------------------------------------------
+router.post('', async function(req, res) {
+	
+	// find the user
+	let user = await utente.findOne({
+		email: req.body.email
+	}).exec();
+	
+	// user not found
+	if (!user) {
+		res.json({ success: false, message: 'Authentication failed. User not found.' });
+		return;
+	}
+	
+
+    // calculate hash
+    let salt = crypto.randomBytes(16).toString('hex');
+    let hash = crypto.pbkdf2Sync(req.body.password, salt, 1000, 64, `sha512`).toString(`hex`); 
+	// check if password hash matches
+	if (user.password != hash) {
+		res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+	}
+	
+	// if user is found and password is right create a token
+	var payload = {
+		email: user.email,
+		id: user._id
+		// other data encrypted in the token	
+	}
+	var options = {
+		expiresIn: 86400 // expires in 24 hours
+	}
+	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+	res.json({
+		success: true,
+		message: 'Enjoy your token!',
+		token: token,
+		email: user.email,
+		id: user._id,
+		self: "api/v1/" + user._id
+	});
+
+});
+
+
+
+module.exports = router;
