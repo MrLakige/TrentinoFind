d432cd94c222f7a698dcf0f1f6538c5281fb0234
Luca <luca01caccavale@gmail.com>
2023-05-16 10:04:39 +0200

    modified:   code/app/utenti.js

--- code/app/utenti.js
+++ code/app/utenti.js
@@ -16,8 +16,18 @@ class Utente{
     description(){
         return this.firstname + "," + this.lastname + "," + this.age;
     }
-    Autenticazione(){}
+    /**
+     * Registrazione()
+     * Questo metodo si occupa di verificare se è possibile effettuare una 
+     * registrazione di un utente all'interno del sistema, in particolare
+     * verificando se l'utente è già stato registrato (verificando se 
+     * l'indirizzo email è già presente), e se i dati immessi sono dati 
+     * validi.
+     */
     Registrazione(){}
+    
+    
+    Autenticazione(){}
     VisuallizzaInformazioni(){}
     ModificaInformazioni(){}
 }
