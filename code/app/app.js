45e8f7821e6f6b8f5fe3261a5d26964c4c8f43f1
Luca <luca01caccavale@gmail.com>
2023-05-18 19:04:35 +0200

    modified:   code/app/app.js

--- code/app/app.js
+++ code/app/app.js
@@ -6,8 +6,8 @@ const app = express();
 //Routing:
 const giocatori = require('./giocatori.js');
 
-// parsing middelware json file
-app.use(express.json());
+
+app.use(express.json());// parsing middelware json file
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/giocatori', giocatori);
@@ -20,6 +20,7 @@ app.use((req,res,next) => {
     console.log(req.method + ' ' + req.url)
     next()
 })
+
 // Default 404 handler 
 app.use((req, res) => {
     res.status(404);
