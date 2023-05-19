3671303c9a292f570a879b11b440d6248aadc5a1
MrLakige <Callegari.Michele01@gmail.com>
2023-05-19 15:30:32 +0200

    modified:   code/app/authentication.js 	new file:   code/app/tokenChecker.js
    author Michele Callegari
    
    ho modificato il file code/app/authentication.js, in modo da gestire gli errori
    ho creato il file code/app/tokenChecker.js serve per validare il token

--- code/app/authentication.js
+++ code/app/authentication.js
@@ -10,44 +10,48 @@ const crypto = require('crypto');
 // ---------------------------------------------------------
 router.post('', async function(req, res) {
 	
-	// find the user
-	let user = await utente.findOne({
-		email: req.body.email
-	}).exec();
-	
-	// user not found
-	if (!user) {
-		res.json({ success: false, message: 'Authentication failed. User not found.' });
-		return;
-	}
-	
+	try {
+			// find the user
+		let user = await utente.findOne({
+			email: req.body.email
+		}).exec();
+		
+		// user not found
+		if (!user) {
+			res.json({ success: false, message: 'Authentication failed. User not found.' });
+			return;
+		}
+		
+		// calculate hash
+		let hash = crypto.pbkdf2Sync(req.body.password, '', 1000, 64, `sha512`).toString(`hex`); 
+		// check if password hash matches
+		if (user.password != hash) {
+			res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+		}
+		
+		// if user is found and password is right create a token
+		var payload = {
+			email: user.email,
+			id: user._id
+			// other data encrypted in the token	
+		}
+		var options = {
+			expiresIn: 86400 // expires in 24 hours
+		}
+		var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
-    // calculate hash
-    let hash = crypto.pbkdf2Sync(req.body.password, '', 1000, 64, `sha512`).toString(`hex`); 
-	// check if password hash matches
-	if (user.password != hash) {
-		res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-	}
-	
-	// if user is found and password is right create a token
-	var payload = {
-		email: user.email,
-		id: user._id
-		// other data encrypted in the token	
-	}
-	var options = {
-		expiresIn: 86400 // expires in 24 hours
-	}
-	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
-
-	res.json({
-		success: true,
-		message: 'Enjoy your token!',
-		token: token,
-		email: user.email,
-		id: user._id,
-		self: "api/v1/" + user._id
-	});
+		res.json({
+			success: true,
+			message: 'Token Create',
+			token: token,
+			email: user.email,
+			id: user._id,
+			self: "api/v1/" + user._id
+		});
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: 'Server error' });
+    }
 
 });
 
