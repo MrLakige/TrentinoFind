12866cb2393fe73a85f382309ad740039c2d895d
Luca <luca01caccavale@gmail.com>
2023-05-27 10:01:46 +0200

    modified:   backend/app/models/schemaCommento.js     Giorno: 27/05/2023     User Story:         Numero: 14         Nome: Inserimento commento     Commenti: Modifica dello schema commento con Modifica dei               nomi dei campi e inserimento               delle istruzione per la creazione del modello moongose

--- backend/app/models/schemaCommento.js
+++ backend/app/models/schemaCommento.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
-exports.user = new mongoose.Schema({
-    idPlayer: String,
-    idObject: String,
-    date: Date,
-    comment: String
+const Schema = mongoose.Schema;
+
+//Creazione dello schema commento
+const schemaCommento = new Schema({
+    idGiocatore: String,
+    idOggetto: String,
+    data: Date,
+    testo: String
 });
+
+//Creazione del modello mongoose, con il quale interagire col database
+const modelloCommento = mongoose.model('Commento', schemaCommento);
+
+modelloCommento.createCollection().then(function(collection) {
+  console.log('Collection Commento is created!');
+});
+
+module.exports = modelloCommento;
