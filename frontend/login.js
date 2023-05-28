d6fc75ea808bf50fbf4986605e90e6ca5867b046
leouni23 <127280054+leouni23@users.noreply.github.com>
2023-05-28 11:38:16 +0200

    Creazione file struttura frontend 	modified:   backend/package-lock.json     modified:   backend/package.json 	new file:   frontend copy/index.html 	new file:   frontend copy/index.js 	new file:   frontend/index.html 	new file:   frontend/login.html 	new file:   frontend/login.js 	new file:   frontend/registration.html 	new file:   frontend/registration.js

---
+++ frontend/login.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+      
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/api/v1/authentication', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          var response = JSON.parse(xhr.responseText);
+          
+          if (xhr.status === 200) {
+            // Successful login
+            document.getElementById('message').innerHTML = 'Login successful!';
+            // Redirect to another page or perform any other actions
+          } else {
+            // Failed login
+            document.getElementById('message').innerHTML = 'Login failed. Please try again.';
+          }
+        }
+      };
+      
+      var data = JSON.stringify({ email: email, password: password });
+      xhr.send(data);
+    });
+  });
+  
